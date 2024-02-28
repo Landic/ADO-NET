7ae0cc32e38e2c962dcfa87e88081daa41c06807
Landic <danil.volkov.235@gmail.com>
2024-02-28 13:07:48 +0200

    hw done

---
+++ Volkov_HW_ADO_NET_6/Volkov_HW_ADO_NET_6/Program.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Volkov_HW_ADO_NET_6
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string City { get; set; }
+        }
+
+        public static void Output(string output, IEnumerable<Person> person)
+        {
+            Console.WriteLine(output);
+            foreach (var i in person)
+            {
+                Console.WriteLine($"{i.Name}, {i.Age}, {i.City}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>()
+            {
+                new Person(){ Name = "Andrey", Age = 24, City = "Kyiv"},
+                new Person(){ Name = "Liza", Age = 18, City = "Odesa" },
+                new Person(){ Name = "Oleg", Age = 15, City = "London" },
+                new Person(){ Name = "Sergey", Age = 55, City = "Kyiv" },
+                new Person(){ Name = "Sergey", Age = 32, City = "Lviv" }
+            };
+
+            var older25 = person.Where(i => i.Age > 25);
+            Output("Старше 25:", older25);
+
+
+            var Not_In_London = person.Where(i => i.City != "London");
+            Output("\n\nНаходящиеся не в Лондоне:", Not_In_London);
+
+
+
+            var kyiv = person.Where(i => i.City == "Kyiv").Select(i => i.Name);
+            Console.WriteLine("\n\nКиев:");
+            foreach (var i in kyiv)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            var Older_35_Sergeys = person.Where(i => i.Name == "Sergey" && i.Age > 35);
+            Output("\n\nСергеи старше 35:", Older_35_Sergeys);
+
+
+            var Live_Odessa = person.Where(i => i.City == "Odesa");
+            Output("\n\nЖивущие в Одессе:", Live_Odessa);
+        }
+    }
+}
---
+++ Volkov_HW_ADO_NET_6_2/Volkov_HW_ADO_NET_6_2/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace Volkov_HW_ADO_NET_6_2
+{
+    internal class Program
+    {
+        class Employee
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public int DepId { get; set; }
+        }
+        class Department
+        {
+            public int Id { get; set; }
+            public string Country { get; set; }
+            public string City { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Department> departments = new List<Department>()
+            {
+                new Department(){ Id = 1, Country = "Ukraine", City = "Lviv" },
+                new Department(){ Id = 2, Country = "Ukraine", City = "Kyiv" },
+                new Department(){ Id = 3, Country = "France", City = "Paris" },
+                new Department(){ Id = 4, Country = "Ukraine", City = "Odesa" }
+             };
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee()
+                { Id = 1, FirstName = "Tamara", LastName = "Ivanova", Age = 22, DepId = 2 },
+                new Employee()
+                { Id = 2, FirstName = "Nikita", LastName = "Larin", Age = 33, DepId = 1 },
+                new Employee()
+                { Id = 3, FirstName = "Alica", LastName = "Ivanova", Age = 43, DepId = 3 },
+                new Employee()
+                { Id = 4, FirstName = "Lida", LastName = "Marusyk", Age = 22, DepId = 2 },
+                new Employee()
+                { Id = 5, FirstName = "Lida", LastName = "Voron", Age = 36, DepId = 4 },
+                new Employee()
+                { Id = 6, FirstName = "Ivan", LastName = "Kalyta", Age = 22, DepId = 2 },
+                new Employee()
+                { Id = 7, FirstName = "Nikita", LastName = " Krotov ", Age = 27, DepId = 4 }
+            };
+
+            var ukrainianOdessa = employees.Where(i => departments.Any(j => j.Id == i.DepId && j.Country == "Ukraine" && j.City != "Odesa")).Select(i => new { i.FirstName, i.LastName });
+            Console.WriteLine("Работники работающие в Украине но не в городе Одесса:");
+            foreach (var i in ukrainianOdessa)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}");
+            }
+
+
+
+
+
+            var Countries = departments.Select(i => i.Country).Distinct();
+            Console.WriteLine("\n\n\nСтраны без повторений:");
+            foreach (var i in Countries)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            var older25 = employees.Where(i => i.Age > 25).Take(3);
+            Console.WriteLine("\n\n\nСтарше 25 работники первые 3:");
+            foreach (var i in older25)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}, {i.Age}");
+            }
+
+
+            var KyivStudents23 = employees.Where(i => departments.Any(j => j.Id == i.DepId && j.City == "Kyiv") && i.Age > 23).Select(i => new { i.FirstName, i.LastName, i.Age });
+            Console.WriteLine("\n\n\nСтуденты старше 23 лет в Киеве:");
+            foreach (var i in KyivStudents23)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}, {i.Age}");
+            }
+
+
+
+
+
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////////");
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            var ukrainianOdessa2 = (from i in employees
+                                                 join j in departments on i.DepId equals j.Id
+                                                 where j.Country == "Ukraine" && j.City != "Odesa"
+                                                 select new { i.FirstName, i.LastName }).ToList();
+            Console.WriteLine("\n\n\nРаботники работающие в Украине но не в городе Одесса:");
+            foreach (var i in ukrainianOdessa2)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}");
+            }
+
+
+
+            var Countries2 = departments.Select(i => i.Country).Distinct().ToList();
+            Console.WriteLine("\n\n\nСтраны без повторений:");
+            foreach (var i in Countries2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            var older252 = employees.Where(i => i.Age > 25).Take(3).ToList();
+            Console.WriteLine("\n\n\nСтарше 25 работники первые 3:");
+            foreach (var i in older252)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}, Age: {i.Age}");
+            }
+
+
+
+
+            var KyivStudents232 = (from i in employees
+                                       join j in departments on i.DepId equals j.Id
+                                       where j.City == "Kyiv" && i.Age > 23
+                                       select new { i.FirstName, i.LastName, i.Age }).ToList();
+
+            Console.WriteLine("\n\n\nСтуденты старше 23 лет в Киеве:");
+            foreach (var i in KyivStudents232)
+            {
+                Console.WriteLine($"{i.FirstName} {i.LastName}, Age: {i.Age}");
+            }
+        }
+    }
+}

d1a93757c9219da2a83576d2304f126fb23bb746
Landic <112880105+Landic@users.noreply.github.com>
2024-03-08 01:08:08 +0200

    hw done

--- Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/Country.cs
+++ Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/Country.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Volkov_HW_ADO_NET_7
 {
-    internal class Country
+    public class Country
     {
         public int ID {  get; set; }
         public string Name { get; set; }
--- Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/CountryDBContext.cs
+++ Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/CountryDBContext.cs
@@ -7,13 +7,12 @@ using System.Threading.Tasks;
 
 namespace Volkov_HW_ADO_NET_7
 {
-    internal class CountryDBContext : DbContext
+    public class CountryDBContext : DbContext
     {
-
         public DbSet<Country> Countries { get; set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=DESKTOP-1147B1S;Database=Countrys;Integrated Security=SSPI;TrustServerCertificate=true");
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=DESKTOP-KBKU3U8;Database=Countrys;Integrated Security=SSPI;TrustServerCertificate=true");
         }
     }
 }

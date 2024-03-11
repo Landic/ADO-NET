8e2e43a62049d35bd50942d5d89b0fa0d0f00ef9
Landic <112880105+Landic@users.noreply.github.com>
2024-03-11 23:49:06 +0200

    hw done

--- Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/obj/Debug/net8.0/Volkov_HW_ADO_NET_7.AssemblyInfo.cs
+++ Volkov_HW_ADO_NET_7/Volkov_HW_ADO_NET_7/obj/Debug/net8.0/Volkov_HW_ADO_NET_7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Volkov_HW_ADO_NET_7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051748e12876f6fe86ad769fe81b417a239b4fb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a93757c9219da2a83576d2304f126fb23bb746")]
 [assembly: System.Reflection.AssemblyProductAttribute("Volkov_HW_ADO_NET_7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Volkov_HW_ADO_NET_7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
